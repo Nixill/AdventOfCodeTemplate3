a908aac3301c8bbbceb010d9e68b69b250de7fec
Steven <github@steven-h.net>
2023-12-03 12:35:05 -0500

    Breaking update
    Add "string fname" to the Part functions

--- src/days/Day1.cs
+++ src/days/Day1.cs
@@ -1,11 +1,11 @@
 public static class Day1
 {
-  public static string Part1(StreamReader input)
+  public static string Part1(string fname, StreamReader input)
   {
     return "";
   }
 
-  public static string Part2(StreamReader input)
+  public static string Part2(string fname, StreamReader input)
   {
     return "";
   }
